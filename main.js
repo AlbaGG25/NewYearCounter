cdc85087cfa8b4de97d430e13812fcdf7fb1ca82
“Alba” <albaginesgarcia25@gmail.com>
2023-12-28 17:34:39 +0100

    counter

---
+++ main.js
@@ -0,0 +1,61 @@
+'use strict'; 
+
+
+let counter = 12;
+let temp;
+
+const btn = document.querySelector('.js-start');
+let uvas = ['🎊','🎊','🎊','🎊','🎊','🎊','🎊','🎊','🎊','🎊','🎊','🎊']; 
+const counterTime = document.querySelector('.time'); 
+const grapesList =document.querySelector('.js-list');
+const msg= document.querySelector('.js-msg');
+const sectionCircle = document.querySelector('.circle');
+const sectionFw =document.querySelector ('.msg_section');
+const main = document.querySelector ('.main');
+
+const renderList = ()=> {
+    let container = '';
+      uvas
+      .map((item)=> {container+=`<li> ${item} </li>`})
+      .reverse("")
+      grapesList.innerHTML= container;
+      return container;
+  }
+renderList();
+
+const handleClick = () =>{
+    const decrementAndShowTime = () => {
+        counter--;
+        counterTime.innerHTML = counter; 
+        if (counter >= 0) {
+            counterTime.innerHTML= counter;
+            uvas.shift();
+            renderList();
+            }else{
+            clearInterval(temp);
+            counter = 12;
+            counterTime.innerHTML=counter;
+            uvas = ['🎊','🎊','🎊','🎊','🎊','🎊','🎊','🎊','🎊','🎊','🎊','🎊']; 
+            renderList();
+            msg.innerHTML = `<article class="msg_article">
+            <h1 class= "title">Happy New Year!!!</h1>
+            </article>`; 
+            msg.classList.remove('hidden');
+            sectionCircle.classList.add ('hidden');
+            main.classList.add('img-fire')
+          }
+    }
+    
+    temp = setInterval (decrementAndShowTime, 1000) 
+    
+}
+
+function restorePage () {
+  sectionCircle.classList.remove('hidden');
+  msg.classList.add ('hidden');
+  main.classList.remove('img-fire');
+}
+
+
+btn.addEventListener ('click', handleClick);
+sectionFw.addEventListener ('click', restorePage);
